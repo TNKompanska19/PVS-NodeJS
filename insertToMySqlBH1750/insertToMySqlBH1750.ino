4bab8c4f0636821df6c28392accafc445b670dc5
Теодора Николаева Компанска (CPA) <TNKompanska19@codingburgas.bg>
2023-12-21 11:54:43 +0200

    Fix ino file

--- insertToMySqlBH1750/insertToMySqlBH1750.ino
+++ insertToMySqlBH1750/insertToMySqlBH1750.ino
@@ -1,86 +1,98 @@
+#include <MySQL_Generic.h>
+#include <MySQL_Generic.hpp>
+ 
 #include <Wire.h>
 #include <SPI.h>
-#include <MySQL_Generic.h>
-#include <BH1750.h>  
-BH1750 lightMeter;
-// Defining BME280 sensor
-#define SEALEVELPRESSURE_HPA (1013.25)
+#include "DHT.h"
 
-
+DHT dht2(2,DHT11);
+ 
 // MySQL Debug Level from 0 to 4
-#define _MYSQL_LOGLEVEL_      1
-#define MYSQL_DEBUG_PORT      Serial
-
+//#define _MYSQL_LOGLEVEL_      1
+//#define MYSQL_DEBUG_PORT      Serial
+#define DHTPIN 2
+ 
 // Network Info
 char ssid[] = "OS";
 char pass[] = "123456789";
-
-// Database Address
+ 
+ 
+// Database Adress
 char server[] = "192.168.0.1";
 uint16_t server_port = 3306;
-
+ 
+ 
 // Database Account
 char user[] = "html";
 char password[] = "password";
+ 
 
 //Database Name
 char database[] = "sensors";
 char table[] = "meteorological_station";
-
+ 
 // Default insert data values
-double temperature = 0;
-double humidity = 0;
-
+int distance;
+ 
 // MySQL something
 MySQL_Connection conn((Client *)&client);
 MySQL_Query *query_mem;
-
+ 
 void setup()
 {
-   
+  // Initialize status LED
+  pinMode(LED_BUILTIN, OUTPUT);
   // Setup Serial
-  Serial.begin(9600);
-  Wire.begin();
+  Serial.begin(115200);
+  dht2.begin();
   // Begin WiFi section
   Serial.println("================================================");
   Serial.println(String("Connecting to ") + ssid);
   WiFi.begin(ssid, pass);
-
-  lightMeter.begin();
-  
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-
+ 
   // Print out info about the WiFi connection
   Serial.print("Connected to network. My IP address is: ");
   Serial.println(WiFi.localIP());
-
+ 
   // Test MySQL connection
   Serial.print("Connecting to SQL Server @ ");
   Serial.print(server);
   Serial.println(String(", Port = ") + server_port);
   Serial.println(String("User = ") + user + String(", PW = ") + password + String(", DB = ") + database);
-
 }
-
-
+ 
+ 
 void loop()
 {
-
-  float lux = lightMeter.readLightLevel();
-  Serial.print("Light: ");
-  Serial.print(lux);
-  Serial.println(" lx");
-  delay(1000);
-
+  // Turn on status LED
+  digitalWrite(LED_BUILTIN, LOW);
+ 
+  // Measuring data
   Serial.println("================================================");
-
+  Serial.println("Measuring data...");
+  delay(500);
+  Serial.println();
+ 
+   float h = dht2.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht2.readTemperature();
+ 
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  Serial.println("Temperature in C:");  
+  Serial.println(dht2.computeHeatIndex(t, h, false));
+ 
+  Serial.println("================================================");
+ 
   // change Insert Query with new data
-  String INSERT_SQL = String("INSERT INTO ") + database + "." + table + " (light) VALUES (" + lux +  ")";
-
+  //String INSERT_SQL = String("UPDATE TABLE ") + database + "." + table + " (light, temperature, humidity) VALUES (" + light + "," + dht2.computeHeatIndex(t, h, false) + "," + h +")";
+  String UPDATE_SQL 
   // Insert data in to database
   Serial.println("Connecting to MySQL...");
   Serial.println();
@@ -89,11 +101,11 @@ void loop()
     delay(500);
     // Initiate the query class instance
     MySQL_Query query_mem = MySQL_Query(&conn);
-
+ 
     if (conn.connected())
     {
       Serial.println(INSERT_SQL);
-
+ 
       // Execute the query
       // KH, check if valid before fetching
       if ( !query_mem.execute(INSERT_SQL.c_str()) )
@@ -111,10 +123,10 @@ void loop()
   {
     Serial.println("\nConnect failed. Trying again on next iteration.");
   }
-
+ 
   // Turn off status LED
   digitalWrite(LED_BUILTIN, HIGH);
-
+ 
   // Sleep till next data measure and insert
   Serial.println("================================================");
   Serial.println("\nSleeping...");
